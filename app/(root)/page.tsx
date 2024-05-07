5e101775413e7b3e49b33eedf782ce7601e2468f
Techie Mike <mngene4@gmail.com>
2024-05-07 23:46:45 +0100

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,34 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 export default function Home() {
+
+  const authUser = {firstName: 'Michael', lastName: 'Techie', email: 'techie@gmail.com'};
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type='greeting'
+            title='Welcome'
+            user={authUser?.firstName || 'Guest'}
+            subtext='Access and Manage your account and transactions efficiently.'
+          />
+          <TotalBalanceBox 
+            accounts = {[]}
+            totalBanks= {1}
+            totalCurrentBalance = {1350.50}
+          />
+        </header>
+      </div> 
+      <RightSidebar
+        user={authUser} 
+        transactions={[]}
+        banks={[{currentBalance: 372.28}, {currentBalance: 9382.29}]}
+      />
+    </section> 
   )
 }
---
+++ components/Sidebar.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+export default function Sidebar({user}: SiderbarProps) {
+    const pathName = usePathname();
+  return (
+    <section className='sidebar'>
+        <nav className='flex flex-col gap-4'>
+            <Link href='/' className='mb-12 flex cursor-pointer items-center gap-2'>
+                <Image src='/icons/logo.svg' width={34} height={34} alt='BankLogo' className='size-[24px] max-xl:size-14' />
+                <h1 className='sidebar-logo'>TechieBank</h1>
+            </Link>
+            {sidebarLinks.map((item) => {
+                const isActive = pathName === item.route || pathName.startsWith(`${item.route}/`);
+                return(
+                    <Link className={cn ('sidebar-link', {'bg-bank-gradient':isActive})} href={item.route} key={item.label}>
+                        <div className='relative size-6'>
+                            <Image 
+                                src={item.imgURL} 
+                                alt={item.label} 
+                                fill 
+                                className={cn ({'brightness-[3] invert-0': isActive})} 
+                            />
+                        </div>
+                        <p className={cn ('sidebar-label', {'!text-white': isActive})}>{item.label}</p>
+                    </Link>
+                )
+            })}
+
+            
+        </nav>
+    </section>
+  )
+}
